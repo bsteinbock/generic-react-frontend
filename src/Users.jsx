a15b36e68c4af7b8b675ef108f484e2d15124a7f
Bill Steinbock <bsteinbock@microtraks.com>
2025-01-07 20:52:42 -0500

    wip - adding a table

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,53 +1,164 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useMemo } from 'react';
 import UserContext from './UserContext.jsx';
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+
+// Person = {
+//  firstName: string
+//  lastName: string
+//  age: number
+//  visits: number
+//  status: string
+//  progress: number
+//}
+
+const defaultData = [
+  {
+    firstName: 'tanner',
+    lastName: 'linsley',
+    age: 24,
+    visits: 100,
+    status: 'In Relationship',
+    progress: 50,
+  },
+  {
+    firstName: 'tandy',
+    lastName: 'miller',
+    age: 40,
+    visits: 40,
+    status: 'Single',
+    progress: 80,
+  },
+  {
+    firstName: 'joe',
+    lastName: 'dirte',
+    age: 45,
+    visits: 20,
+    status: 'Complicated',
+    progress: 10,
+  },
+];
+
+const columnHelper = createColumnHelper();
+
+const columns = [
+  columnHelper.accessor('firstName', {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor((row) => row.lastName, {
+    id: 'lastName',
+    cell: (info) => <i>{info.getValue()}</i>,
+    header: () => <span>Last Name</span>,
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('age', {
+    header: () => 'Age',
+    cell: (info) => info.renderValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('visits', {
+    header: () => <span>Visits</span>,
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('status', {
+    header: 'Status',
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('progress', {
+    header: 'Profile Progress',
+    footer: (info) => info.column.id,
+  }),
+];
 
 const Users = () => {
-  const { authToken } = useContext(UserContext);
-  const [users, setUsers] = useState([]);
+  const [data, _setData] = React.useState(() => [...defaultData]);
+  const rerender = React.useReducer(() => ({}), {})[1];
 
-  // Fetch todos from the backend
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await fetch('http://localhost:5050/api/v1/users', {
-          headers: {
-            Authorization: `Bearer ${authToken}`,
-          },
-        });
-
-        const data = await response.json();
-        setUsers(data);
-      } catch (error) {
-        console.error('There was an error fetching todos!', error);
-      }
-    };
-
-    fetchUsers();
-  }, []);
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   return (
-    <>
-      <div className="todo-list-header">
-        <h2>Users</h2>
-        <div className="todo-header-image">
-          <svg height="24px" width="24px" fill="#5f6368">
-            <use href="#users" />
-          </svg>
-        </div>
-      </div>
-
-      <div className="todo-list-container">
-        <ul className="list-items">
-          {users.map((user) => (
-            <li key={user.id}>
-              <div className="user-name">{user.fullName}</div>
-              <div className="user-email">{user.email}</div>
-            </li>
+    <div className="p-2">
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
           ))}
-        </ul>
-      </div>
-    </>
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          {table.getFooterGroups().map((footerGroup) => (
+            <tr key={footerGroup.id}>
+              {footerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.footer,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+      <div className="h-4" />
+      <button onClick={() => rerender()} className="border p-2">
+        Rerender
+      </button>
+    </div>
   );
 };
+
+// Fetch todos from the backend
+// useEffect(() => {
+//   const fetchUsers = async () => {
+//     try {
+//       const response = await fetch('http://localhost:5050/api/v1/users', {
+//         headers: {
+//           Authorization: `Bearer ${authToken}`,
+//         },
+//       });
+//
+//       const data = await response.json();
+//       setUsers(data);
+//     } catch (error) {
+//       console.error('There was an error fetching todos!', error);
+//     }
+//   };
+//
+//   fetchUsers();
+// }, []);
+//
 
 export default Users;
